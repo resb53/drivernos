3970133f11dd86414b9997618b690e79f900dd19
resb53 <resb53.git@gmail.com>
2022-03-03 19:25:41 +0000

    Initialisation script added

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"We have logged in as {client.user}")
 
 
 @client.event
@@ -14,8 +14,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(".init"):
+        if not message.author.guild_permissions.administrator:
+            await message.channel.send("This command can only be run by server admins.")
+
+        else:
+            numchan = message.content[6:].strip(" ")
+
+            if numchan in [c.name for c in message.guild.text_channels]:
+                await message.channel.send(f"Initialising DriverNos in channel {numchan}.")
+            else:
+                await message.channel.send(f"Text channel {numchan} does not exist.")
 
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv("TOKEN"))
