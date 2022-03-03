ba85af81b09fda218b54a178e5193bdc919e5d8b
resb53 <resb53.git@gmail.com>
2022-03-03 22:37:33 +0000

    Create blank driver number template

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import discord
 import os
+from collections import defaultdict
 
 client = discord.Client()
+
+blank_template = ["", ""]
+for i in range(1, 10):
+    blank_template[0] += f"` {i}` - \n"
+for i in range(10, 50):
+    blank_template[0] += f"`{i}` - \n"
+for i in range(50, 100):
+    blank_template[1] += f"`{i}` - \n"
 
 
 @client.event
@@ -17,14 +26,32 @@ async def on_message(message):
     if message.content.startswith(".init"):
         if not message.author.guild_permissions.administrator:
             await message.channel.send("This command can only be run by server admins.")
+            return
 
-        else:
-            numchan = message.content[6:].strip(" ")
+        numchan = message.content[6:].strip(" ")
 
-            if numchan in [c.name for c in message.guild.text_channels]:
-                await message.channel.send(f"Initialising DriverNos in channel {numchan}.")
-            else:
-                await message.channel.send(f"Text channel {numchan} does not exist.")
+        if len(numchan) == 0:
+            await message.channel.send("Please provide a channel name to initialise. e.g: `.init numbers`")
+            return
+
+        textchannels = defaultdict(list)
+
+        for tchans in message.guild.text_channels:
+            textchannels[tchans.name].append(tchans.id)
+
+        if numchan not in textchannels:
+            await message.channel.send(f"Text channel `{numchan}` does not exist.")
+            return
+        elif len(textchannels[numchan]) > 1:
+            await message.channel.send(f"Multiple `{numchan}` channels exist. Choose a unique channel name.")
+            return
+
+        numchanid = message.guild.get_channel(textchannels[numchan][0])
+
+        for msg in blank_template:
+            await numchanid.send(msg)
+
+        return
 
 
 client.run(os.getenv("TOKEN"))
